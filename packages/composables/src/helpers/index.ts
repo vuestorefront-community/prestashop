11f7638c56f8fdc9e3ad9bc38442469a793b134c
Sam <sam@binshops.com>
2021-09-23 16:31:40 -0500

    adds populate products, types and changes product getter

---
+++ packages/composables/src/helpers/index.ts
@@ -0,0 +1,9 @@
+import populateCategoryProducts from './populateCategoryProducts';
+import populateFeaturedProducts from './populateFeaturedProducts';
+import populateProducts from './populateProducts';
+
+export {
+  populateCategoryProducts,
+  populateFeaturedProducts,
+  populateProducts
+};
---
+++ packages/composables/src/helpers/populateCategoryProducts.ts
@@ -0,0 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const populateCategoryProducts = (psProducts: Array<any>) => {
+  const populateCategoryProducts = psProducts.map((product) => ({
+    id: product.id_product,
+    name: product.name,
+    slug: product.link_rewrite,
+    regularPrice: product.regular_price_amount,
+    discountPrice: product.price_amount,
+    coverImageSmall: product.cover?.url,
+    coverImageMedium: product.cover?.url,
+    coverImageLarge: product.cover?.url,
+    images: [],
+    description: null,
+    shortDescription: null,
+    brand: null,
+    category: null,
+    productInfo: null
+  }));
+
+  return populateCategoryProducts;
+};
+
+export default populateCategoryProducts;
