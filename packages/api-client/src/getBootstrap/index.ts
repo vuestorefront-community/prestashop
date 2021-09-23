9e9e539b7abe90176219b0b57f85ec2ae059720a
Sam <sam@binshops.com>
2021-09-23 10:40:18 -0500

    feat: adds bootstrap pai

---
+++ packages/api-client/src/getBootstrap/index.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export async function getBootstrap(context) {
+  const url = new URL('/rest/lightbootstrap', context.config.api.url);
+
+  url.searchParams.set('menu_with_images', 'single');
+
+  const { data } = await context.client.get(url.href);
+
+  return data;
+}
