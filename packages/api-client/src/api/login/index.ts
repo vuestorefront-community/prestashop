17855fc64ab389bd109e19b3fcb15b441dcc551a
Sam <sam@binshops.com>
2021-11-09 12:28:21 -0600

    cart - after login

--- packages/api-client/src/api/login/index.ts
+++ packages/api-client/src/api/login/index.ts
@@ -3,11 +3,15 @@ import {cookieParser} from '../../helpers/cookieParser';
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default async function login(context, params) {
   const url = new URL('/rest/login', context.config.api.url);
-  const { username, password } = params;
+  const { username, password, psCookieKey, psCookieValue } = params;
 
   const { data, headers } = await context.client.post(url.href, {
     email: username,
     password: password
+  }, {
+    headers: {
+      Cookie: psCookieKey + '=' + psCookieValue + ';'
+    }
   });
 
   const cookieObject = cookieParser(headers);
