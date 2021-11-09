a401f7fe92ce6fcd27f6f82032be51ad87affabd
Sam <sam@binshops.com>
2021-11-08 19:19:55 -0600

    adapts api-client to new Ps Cart API

--- packages/api-client/src/api/getCartItems/index.ts
+++ packages/api-client/src/api/getCartItems/index.ts
@@ -3,7 +3,7 @@ import {cookieParser} from '../../helpers/cookieParser';
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default async function getCartItems(context, params) {
   const {psCookieKey, psCookieValue} = params;
-  const url = new URL('/rest/cartitems', context.config.api.url);
+  const url = new URL('/rest/cart', context.config.api.url);
 
   url.searchParams.set('image_size', 'medium_default');
 
