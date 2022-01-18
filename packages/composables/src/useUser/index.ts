4b6c57ae8f35a9b8a6d22d9d0930706a8f5d59c4
Sam <sam@binshops.com>
2022-01-18 17:42:30 -0600

    user data after login

--- packages/composables/src/useUser/index.ts
+++ packages/composables/src/useUser/index.ts
@@ -105,7 +105,7 @@ const params: UseUserFactoryParams<User, UpdateParams, RegisterParams> = {
       // authentication failed
     }
 
-    return {};
+    return data.psdata.user;
   },
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
