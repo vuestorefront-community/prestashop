4f4aac8f43fcb07d29acf2bd8f878f792322d52d
Sam <sam@binshops.com>
2022-01-19 15:07:50 -0600

    checkout steps

--- packages/theme/middleware/checkout.js
+++ packages/theme/middleware/checkout.js
@@ -3,21 +3,34 @@ export default async ({ app, $vsf }) => {
   const currentPath = app.context.route.fullPath.split('/checkout/')[1];
   console.log(currentPath);
 
-  if (currentPath === 'payment') {
-    const vsfCookieKey = $vsf.$prestashop.config.app.$config.psCustomerCookieKey;
-    const vsfCookieValue = $vsf.$prestashop.config.app.$config.psCustomerCookieValue;
+  const vsfCookieKey = $vsf.$prestashop.config.app.$config.psCustomerCookieKey;
+  const vsfCookieValue = $vsf.$prestashop.config.app.$config.psCustomerCookieValue;
 
-    const psCookieKey = $vsf.$prestashop.config.app.$cookies.get(vsfCookieKey);
-    const psCookieValue = $vsf.$prestashop.config.app.$cookies.get(vsfCookieValue);
+  const psCookieKey = $vsf.$prestashop.config.app.$cookies.get(vsfCookieKey);
+  const psCookieValue = $vsf.$prestashop.config.app.$cookies.get(vsfCookieValue);
 
-    const { data } = await $vsf.$prestashop.api.getPaymentMethods({ psCookieKey, psCookieValue });
+  switch (currentPath) {
+    case 'payment':
+      const customer = await $vsf.$prestashop.api.loadCustomer({psCookieKey, psCookieValue});
+      if (customer.code === 410) {
+        app.context.redirect('/checkout/user-account');
+      }
 
-    if (data.code === 200) {
-      if (Object.keys(data.psdata).length === 0) {
+      const { data } = await $vsf.$prestashop.api.getPaymentMethods({ psCookieKey, psCookieValue });
+
+      if (data.code === 200) {
+        if (Object.keys(data.psdata).length === 0) {
+          app.context.redirect('/checkout/shipping');
+        }
+      } else {
         app.context.redirect('/checkout/shipping');
       }
-    } else {
-      app.context.redirect('/checkout/shipping');
-    }
+      break;
+    case 'shipping':
+      const result = await $vsf.$prestashop.api.loadCustomer({psCookieKey, psCookieValue});
+      if (result.code === 410) {
+        app.context.redirect('/checkout/user-account');
+      }
+      break;
   }
 };
