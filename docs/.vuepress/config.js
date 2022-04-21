b032adf53d84369d24cbfd3cd25f6a482a32c090
Sam <sam@binshops.com>
2022-04-21 19:58:38 -0400

    chore: vuepress dependencies

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -41,7 +41,9 @@ module.exports = {
     logo: 'https://user-images.githubusercontent.com/1626923/137092657-fb398d20-b592-4661-a1f9-4135db0b61d5.png',
     nav: [
       { text: 'Vue Storefront', link: 'https://vuestorefront.io/' },
-      { text: 'Core Documentation', link: 'https://docs.vuestorefront.io/v2/' }
+      { text: 'Core Documentation', link: 'https://docs.vuestorefront.io/v2/' },
+      // { text: 'Demo', link: '' },
+      { text: 'GitHub', link: 'https://github.com/vuestorefront-community/prestashop'},
     ],
     sidebar: [
       {
