9ad115c59037c2c07f79aa03a192b2ee1cfc27a0
acetousk <acetousk@gmail.com>
2022-06-08 19:36:31 -0600

    Refactor: cookieParser.ts to be safer and more consise

--- packages/api-client/src/helpers/cookieParser.ts
+++ packages/api-client/src/helpers/cookieParser.ts
@@ -1,12 +1,9 @@
-import {logger} from './logging';
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const parsePsCookie = (str) =>
   str
-    .split(';')
-    .slice(0, 1)
-    .map(v => v.split('='))
-    .reduce((acc, v) => {
+    ?.split(';')
+    ?.slice(0, 1)
+    ?.map(v => v.split('='))
+    ?.reduce((acc, v) => {
       acc.vsfPsKeyCookie = decodeURIComponent(v[0] ? v[0].trim() : '');
       acc.vsfPsValCookie = decodeURIComponent(v[1] ? v[1].trim() : '');
       return acc;
@@ -15,29 +12,10 @@ const parsePsCookie = (str) =>
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const cookieParser = (headers) => {
   // to get the latest Auth cookie - normally there are two PrestaShop cookies
-  // TODO: This could be done much more efficently
-  if (headers['set-cookie'] || headers['Set-Cookie']) {
-    logger.info(headers);
+  const cookieArray = headers['set-cookie'] ? headers['set-cookie'] : [];
 
-    const numberOfCookies = headers['set-cookie'].length + headers['Set-Cookie'].length;
-    logger.info(numberOfCookies);
-
-    let cookieString = null;
-    for (let i = 0; i < numberOfCookies; i++) {
-      // prestashop cookies start with PrestaShop
-      if (headers['set-cookie'][i].includes('PrestaShop')) {
-        cookieString = headers['set-cookie'][i];
-      } else if (headers['Set-Cookie'][i].includes('JSESSIONID')) {
-        cookieString = headers['Set-Cookie'][i];
-      }
-      logger.info(cookieString);
-    }
-
-    logger.info(parsePsCookie(cookieString));
-    return parsePsCookie(cookieString);
-  } else {
-    return null;
-  }
+  // TODO: Allow for multiple cookies rather than just the first
+  return parsePsCookie(cookieArray?.at(0)) || '';
 };
 
 export {
