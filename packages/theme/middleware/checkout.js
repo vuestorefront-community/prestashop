d1c86211bbc23c97ebb74f2464e061b4c9d97232
Sam <sam@binshops.com>
2022-11-08 07:10:18 -0500

    fix: not redirecting to checkout page

--- packages/theme/middleware/checkout.js
+++ packages/theme/middleware/checkout.js
@@ -8,14 +8,18 @@ export default async ({ app, $vsf }) => {
   const psCookieKey = $vsf.$prestashop.config.app.$cookies.get(vsfCookieKey);
   const psCookieValue = $vsf.$prestashop.config.app.$cookies.get(vsfCookieValue);
 
+  const lang = $vsf.$prestashop.config.app.i18n.locales && $vsf.$prestashop.config.app.i18n.locales.length > 1 ? '/' + $vsf.$prestashop.config.app.$cookies.get('vsf-locale') : '';
+  const currency = $vsf.$prestashop.config.app.$cookies.get('vsf-currency');
+
   switch (currentPath) {
     case 'payment':
-      const customer = await $vsf.$prestashop.api.loadCustomer({key: psCookieKey, value: psCookieValue});
+      const customer = await $vsf.$prestashop.api.loadCustomer({key: psCookieKey, value: psCookieValue, lang: lang, currency: currency});
+
       if (customer.code === 410) {
         app.context.redirect('/checkout/user-account');
       }
 
-      const { data } = await $vsf.$prestashop.api.getPaymentMethods({ psCookieKey, psCookieValue });
+      const { data } = await $vsf.$prestashop.api.getPaymentMethods({ psCookieKey, psCookieValue, lang: lang, currency: currency});
 
       if (data.code === 200) {
         if (Object.keys(data.psdata).length === 0) {
@@ -26,7 +30,7 @@ export default async ({ app, $vsf }) => {
       }
       break;
     case 'shipping':
-      const result = await $vsf.$prestashop.api.loadCustomer({key: psCookieKey, value: psCookieValue});
+      const result = await $vsf.$prestashop.api.loadCustomer({key: psCookieKey, value: psCookieValue, lang: lang, currency: currency});
       if (result.code === 410) {
         app.context.redirect('/checkout/user-account');
       }
