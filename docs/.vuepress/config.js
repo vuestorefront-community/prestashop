94f6aba677916bcda37ba0bba31b422a58534679
Sam <sam@binshops.com>
2022-12-02 19:11:02 -0500

    doc: added v1.1.2 md file

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -89,6 +89,7 @@ module.exports = {
           ['/guide/releases/v1.0.0', 'v1.0.0'],
           ['/guide/releases/v1.1.0', 'v1.1.0'],
           ['/guide/releases/v1.1.1', 'v1.1.1'],
+          ['/guide/releases/v1.1.2', 'v1.1.2'],
         ]
       }
     ]
