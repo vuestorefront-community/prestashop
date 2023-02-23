fd7b38ae59a1509460df884064c2fdad715127b7
Sam <sam@binshops.com>
2023-02-23 03:14:48 -0500

    fix: added locale check to composables

--- packages/composables/src/useFacet/index.ts
+++ packages/composables/src/useFacet/index.ts
@@ -11,7 +11,11 @@ const factoryParams = {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   search: async (context: Context, params: FacetSearchResult<SearchParams>) => {
 
-    const lang = context.$prestashop.config.app.i18n.locales && context.$prestashop.config.app.i18n.locales.length > 1 ? '/' + context.$prestashop.config.app.$cookies.get('vsf-locale') : '';
+    const lang = context.$prestashop.config.app.i18n.locales &&
+    context.$prestashop.config.app.i18n.locales.length > 1 &&
+    context.$prestashop.config.app.$cookies.get('vsf-locale')
+      ? '/' + context.$prestashop.config.app.$cookies.get('vsf-locale') : '';
+
     const currency = context.$prestashop.config.app.$cookies.get('vsf-currency');
 
     const data = await context.$prestashop.api.getCategoryProducts({ ...params, lang: lang, currency: currency });
