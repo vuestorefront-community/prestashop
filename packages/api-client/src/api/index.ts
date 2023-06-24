320b7732e31919c87b865e4a2e47516d5a0f3f0b
shaghayegh <shaghayegh.ahmadi1223@gmail.com>
2023-06-24 15:22:21 +0330

    set whish list load

--- packages/api-client/src/api/index.ts
+++ packages/api-client/src/api/index.ts
@@ -23,3 +23,4 @@ export { default as getPaymentMethods } from './getPaymentMethods';
 export { default as makeOrder } from './makeOrder';
 export { default as updateCustomer } from './updateCustomer';
 export { default as addItemToWishlist } from './addItemToWishlist';
+export { default as getWhishlistItems } from './getWhishlistItems';
